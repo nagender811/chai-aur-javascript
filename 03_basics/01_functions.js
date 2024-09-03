5fc07d39ec75db2e3b96e369d9e795de0b17b883
itznagender <kumarnagender811@gmail.com>
2024-09-03 17:16:15 +0530

    add functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,25 @@
+function sayMyName(){
+    console.log("N");
+    console.log("A");
+    console.log("G");
+    console.log("E");
+    console.log("N");
+    console.log("D");
+    console.log("E");
+    console.log("R");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+        let result = number1 + number2
+        // return result
+        return number1 + number2
+    }
+
+    const result = addTwoNumbers(8, 9)
+    console.log("Result:", result)
