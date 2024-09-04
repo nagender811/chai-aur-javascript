ea8c925d20b356b07bb038ab98a752ab7550045e
itznagender <kumarnagender811@gmail.com>
2024-09-04 17:13:04 +0530

    add control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+// if
+const isUserloggedIn = true
+const temperature = 40
+
+if (temperature === "40") {
+    console.log("Both temp are equal");
+}
+else{
+    console.log("Tempratures are not equal");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,37 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const weekday = 2
+switch (weekday) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+        
+    default:
+        console.log("Invalid Input");
+        break;
+}
