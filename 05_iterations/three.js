4f0ffc301221bce9a4dba65cda9647ca82803f73
itznagender <kumarnagender811@gmail.com>
2024-09-05 21:37:18 +0530

    add iteration example 3

---
+++ 05_iterations/three.js
@@ -0,0 +1,34 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "Unites State of America")
+map.set('Fr', "France")
+map.set('Af', "Africa")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'SpiderMan'
+}
+
+// for (const [key, value] of myObject) {
+//     // console.log(key, ':-', value);
+// }
